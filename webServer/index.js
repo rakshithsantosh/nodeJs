b665f9995f2fc1fb777943ae3ccd9585b899d5e5
rakshith_s <rakshithsantosh@gmail.com>
2024-01-21 22:23:21 +0530

    template

--- webServer/index.js
+++ webServer/index.js
@@ -1,7 +1,21 @@
 const http = require('http')
+const url = require('url')
 
 const server = http.createServer((req,res)=>{
-    res.end("response from the server")
+    const pathName = req.url
+    if(pathName==='/overview'){
+        res.end("this is overview")
+    }
+    else if (pathName === '/product'){
+        res.end("this is product")
+    }
+    else{
+        res.writeHead(404,{
+            'Content-type':'text.html',
+            'my-own-header':'hello world'
+        })
+        res.end("<h1>Page not found</h1>")
+    }
 })
 
 server.listen(8000,'127.0.0.1',()=>{
